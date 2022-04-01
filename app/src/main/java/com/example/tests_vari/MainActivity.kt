d2fa7a78f7e5c30af491713a62e023864a05610c
giuseppegargani <sentierodisole1@yahoo.it>
2022-04-01 16:27:30 +0200

    prima di creazione plugin kotlin

--- app/src/main/java/com/example/tests_vari/MainActivity.kt
+++ app/src/main/java/com/example/tests_vari/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.tests_vari
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.tests_vari.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var helloText: TextView
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        helloText = binding.helloText
+        helloText.text = "accidenti".toString()
+
+        //si legge il dato da un file xml!!!!
+
     }
 }
