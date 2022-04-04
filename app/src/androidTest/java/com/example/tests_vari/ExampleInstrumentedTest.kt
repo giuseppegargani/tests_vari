b5c201b9f676a30d18b7ffb68bf5a7319e0447fb
giuseppegargani <sentierodisole1@yahoo.it>
2022-04-04 18:42:30 +0200

    altra configurazione

--- app/src/androidTest/java/com/example/tests_vari/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tests_vari/ExampleInstrumentedTest.kt
@@ -1,12 +1,23 @@
 package com.example.tests_vari
 
+/* PERMESSI PER TESTING: GRANTPEERMISSIONRULE https://developer.android.com/reference/androidx/test/rule/GrantPermissionRule
+  - @Rule @JvmField val grantPermissions = GrantPermissionRule.grant(
+    android.Manifest.permission.ACCESS_FINE_LOCATION,
+    android.Manifest.permission.ACCESS_COARSE_LOCATION,
+    android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+    richiedono questa dipendenza: androidTestImplementation 'com.android.support.test:rules:1.0.2'
+ */
+
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,6 +26,14 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val grantPermissions:GrantPermissionRule = GrantPermissionRule.grant(
+        android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
