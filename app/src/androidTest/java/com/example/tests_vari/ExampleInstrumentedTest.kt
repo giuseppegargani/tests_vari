f19e457003897745f4d15b5dfefe20b6eec2de33
giuseppegargani <sentierodisole1@yahoo.it>
2022-04-05 03:20:30 +0200

    verifica lettura

--- app/src/androidTest/java/com/example/tests_vari/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tests_vari/ExampleInstrumentedTest.kt
@@ -9,15 +9,23 @@ package com.example.tests_vari
     richiedono questa dipendenza: androidTestImplementation 'com.android.support.test:rules:1.0.2'
  */
 
+import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
+import org.junit.After
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
+import java.io.File
+import java.lang.Exception
+import java.io.FileOutputStream
+import java.io.FileReader
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -26,25 +34,114 @@ import org.junit.Rule
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    private lateinit var nameTest: String
 
-    @Rule
+    @Before
+    fun inizializza(){
+        nameTest="stringa iniziale"
+    }
+
+    //var nameTest: MutableList<String> = mutableListOf()
+
+    //@After
+    //fun scritta() { Log.d("giuseppe", "il nome del test passato e' $nameTest") }
+
+    /*@Rule
     @JvmField
     val grantPermissions:GrantPermissionRule = GrantPermissionRule.grant(
         android.Manifest.permission.ACCESS_FINE_LOCATION,
         android.Manifest.permission.ACCESS_COARSE_LOCATION,
-        android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE)*/
 
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.tests_vari", appContext.packageName)
+    //questa funzione scrive in console sulla base della stringa ed evento
+    fun scriviConsole(nomeTest: String){
+        Log.d("giuseppe", "lanciata scrittura in console")
+        val context= InstrumentationRegistry.getInstrumentation().targetContext
+        val path: File = context.getExternalFilesDir(null)!!
+        Log.d("giuseppe", "nome directory $path")
+        val file = File(path, "giuseppe.txt")
+        val stream = FileOutputStream(file)
+        stream.use { stream ->
+            stream.write("Nome del Test: $nomeTest".toByteArray())
+        }
+    }
+
+    fun leggiConsole():String{
+        Log.d("giuseppe", "lanciata lettura in console")
+        val context= InstrumentationRegistry.getInstrumentation().targetContext
+        val path: File = context.getExternalFilesDir(null)!!
+        Log.d("giuseppe", "nome directory $path")
+        val file = File(path, "giuseppe.txt")
+        /*val stream = FileOutputStream(file)
+        stream.use { stream ->
+            stream.write("Nome del Test: $nomeTest".toByteArray())
+        }*/
+
+        val reader = FileReader(file)
+        val txt = reader.readText()
+        Log.d("giuseppeLettura", "****** stringa letta $txt")
+        reader.close()
+        return txt
     }
 
     @Test
+    fun useAppContext() {
+        nameTest= "useAppContext"
+        try {
+            // Context of the app under test.
+            val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+            assertEquals("com.example.tests_vari", appContext.packageName)
+
+            //una volta verificato si dovrebbe poter scrivere
+            /*val context= InstrumentationRegistry.getInstrumentation().targetContext
+            val path: File = context.getExternalFilesDir(null)!!
+            Log.d("giuseppe", "nome directory $path")
+            val file = File(path, "giuseppe.txt")
+            val stream = FileOutputStream(file)
+            stream.use { stream ->
+                stream.write("Nome del Test: $nameTest".toByteArray())
+            }*/
+            scriviConsole("ecchime")
+            Log.d("giuseppe", "Test $nameTest riuscito!!!")
+        }
+        catch (exception: Exception){
+            Log.d("giuseppe", "Test $nameTest non riuscito!!! e ha generato una eccezione")
+        }
+    }
+
+    //@Test
+    fun useAppContextB() {
+        nameTest+= "useAppContextB"
+        try {
+            // Context of the app under test.
+            val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+            assertEquals("com.example.tests_vari", appContext.packageName)
+            Log.d("giuseppe", "Test $nameTest riuscito!!!")
+        }
+        catch (exception: Exception){
+            Log.d("giuseppe", "Test $nameTest non riuscito!!! e ha generato una eccezione")
+        }
+    }
+
+    //@Test
     fun useAppContext2() {
+        nameTest+="useAppContext2"
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.tests_var", appContext.packageName)
+        try {
+            // Context of the app under test.
+            val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+            assertEquals("com.example.tests_vari", appContext.packageName)
+            nameTest.map { it -> Log.d("giuseppe", " test $it") }
+        }
+        catch (exception: Exception){
+            Log.d("giuseppe", "Test $nameTest non riuscito!!! e ha generato una eccezione")
+        }
+    }
+
+    @Test
+    fun verificaLettura(){
+        var letto = leggiConsole()
+        Log.d("giuseppeLettura", "letto dentro la funzione $letto")
+        assertEquals("Nome del Test: ecchime", letto)
     }
 }
